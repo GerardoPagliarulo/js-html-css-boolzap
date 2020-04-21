d90a4b054adb61cbeb246e6b8b1f4a851f0df15a
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-04-21 21:25:36 +0200

    Boolzap

--- script/main.js
+++ script/main.js
@@ -1,62 +1,116 @@
 $(document).ready(function () {
     // Referenze
-    var actualChat = $('.conversation');
     var newMessage = $('.new-message');
-    var sendBtn = $('.fa-paper-plane');
+    var sendIcon = $('.content-messages-bar .send-icon');
     var searchContact =  $('.side-search input');
-    //var friendsName = $('.side-contacts .avatar-name h5');
+    
+
+
+    /**
+     *  FOCUS E BLUR INPUT
+     */
+    // Focus e Blur input message-bar con cambio di icona
+    newMessage.on('focus blur', function () {
+        sendIcon.toggleClass('fa-microphone fa-paper-plane');
+    });
 
     // Focus input message-bar con cambio di icona
-    newMessage.focus(function () {
-        $('.fa-microphone').addClass('no-active').next().removeClass('no-active');
-    });
+    //newMessage.focus(function () {
+        //sendIcon.addClass('fa-paper-plane');
+    //});
 
     // Blur su input message-bar
     //newMessage.blur( function () {
-        //sendBtn.addClass('no-active').prev().removeClass('no-active');
+        //sendIcon.removeClass('fa-paper-plane');
     //});
 
+
+    /**
+     * INVIO MESSAGGIO
+     */
     // Messaggio inviato con il tasto Enter
     newMessage.keydown(function (e) { 
 
         if (e.which == 13) {
             sendmessage();
 
-            // Risposta dopo 1s
+            // Risposta interlocutore dopo 1s
             setTimeout (risposta, 1000);
         }
     });
 
-    // Messaggio inviato con click su icona "plane"
-    sendBtn.click(function () { 
+    // Messaggio inviato con click su icona "paper-plane"
+    sendIcon.click(function () { 
 
         sendmessage();
 
         // Risposta interlocutore dopo 1s
         setTimeout (risposta, 1000);
+    });        
 
-        sendBtn.addClass('no-active').prev().removeClass('no-active');
-    });
 
-    // Scorrimento automatico della Chat
-    //var chatDisplay = $('.content-display-chat .conversation li');
-    //chatDisplay.scrollTop(chatDisplay.innerHeight());
-
-    // Ricerca Contatti
+    /*
+     * RICERCA CONTATTI
+     */
+    // Ricerca contatti in search input
     searchContact.keyup(function () {
-        $('.side-contacts ul li').hide();
-        var friendsContact = $(this).val().trim().toLowerCase();
-        //console.log(friendsContact);
+        var friendsName = $(this).val().trim().toLowerCase();
+        //console.log(friendsName);
+
+        $('.side-contacts .list-contacts li').hide(); 
 
         $('.side-contacts .avatar-name h5').each(function () {
-            if ($(this, '.side-contacts .avatar-name h5').text().toLowerCase().includes(friendsContact)) {
+            if ($(this, '.side-contacts .avatar-name h5').text().toLowerCase().includes(friendsName)) {
                 $(this).parents().show();
             }
         });
     });
 
+    //searchContact.keyup(function () {
+        //var friendsName = $(this).val().trim().toLowerCase();
+        ////console.log(friendsName);
 
-     // FUNZIONI
+        //$('.list-contacts li').each(function () {
+            //var friendsContact = $(this).find('.avatar-name h5').text().toLowerCase();
+            
+            //if (friendsContact.includes(friendsName)) {
+                //$(this).show();
+            //}
+            //else {
+                //$(this).hide();
+            //}
+        //});
+    //});
+
+
+        /**
+     * SELEZIONE DI UNA CONVERSAZIONE
+     */
+
+    $('.side-contacts .user').click(function() {
+
+        if(!($(this).hasClass('active-profile'))) {
+            $(this).addClass('active-profile');
+            $(this).prevAll('.user').removeClass('active-profile');
+            $(this).nextAll('.user').removeClass('active-profile');
+        }
+
+        var rightMessages = $(this).attr('data-conversation');
+        //console.log(rightMessages);
+
+        $('.right-messages').removeClass('active');
+
+        $('.right-messages[data-conversation="' + rightMessages + '"]').addClass('active');
+
+        // Scroll della chat
+        scrollChat(); 
+    });
+
+
+    /******* 
+    FUNZIONI
+    *******/
+    
     // Funzione: Invio Messaggi
     function sendmessage() {
         var text = newMessage.val().trim();
@@ -68,14 +122,19 @@ $(document).ready(function () {
 
             newMessageWrited.addClass('my-message');
 
+            newMessageWrited.children('.msg-time').text(time());    
+
+            var actualChat = $('.right-messages.active .conversation');
             actualChat.append(newMessageWrited);
 
-            //$('.template li p').text('');
-            newMessage.val('');        
+            newMessage.val(''); 
+            
+            //Scroll della chat
+            scrollChat(); 
         }
     }
 
-    //Funzione: Risposta
+    // Funzione: Risposta interlocutore
     function risposta() {
         $('.template li p').text('Ok');
 
@@ -83,7 +142,35 @@ $(document).ready(function () {
 
         returnMessage.addClass('friends-message');
 
+        returnMessage.children('.msg-time').text(time());    
+
+        var actualChat = $('.right-messages.active .conversation');
         actualChat.append(returnMessage);
+
+        // Scroll della chat
+        scrollChat(); 
     }
 
+    // Creazione e inserimento orario
+    function time() {
+        var data = new Date();
+        var ora = addZero( data.getHours() );
+        var minuti = addZero( data.getMinutes() );
+        return orario = ora + ':' + minuti;
+    }        
+
+    // Aggiungi zero iniziale a numeri inferiori a 10
+    function addZero(numero) {
+        if(numero < 10) {
+            numero = '0' + numero;
+        }
+        return numero;
+    }
+
+    // Funzione: Scorrimento automatico della Chat
+    function scrollChat() {
+        var pixelScroll = $('.right-messages').height();
+        $('.content-display-chat').scrollTop(pixelScroll);    
+    }
+    
 }); // End Ready
